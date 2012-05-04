680d4c2aca04d9be5519effe22133c32ef03469d
Daniel Rodrigues <inf.daniel@gmail.com>
2012-05-04 00:24:13 -0300

    Starting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniBlog::Application.config.secret_token = 'd9133eef860da3efbf57b41f842ab3eb696ce58c8610d739ba26d2ef2b5e0f8fc15b9c82ab930b9fc5efc54eca1cb1489d99de943578bee2d643a2320f3b43da'
